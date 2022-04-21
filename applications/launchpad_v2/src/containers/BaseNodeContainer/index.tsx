dc19bfb3b448f2873ebc9968ce1b978bd69656f9
tarnas <tarnas@altalogy.com>
2022-04-21 11:12:50 +0200

    improved styling with the inverted values

--- applications/launchpad_v2/src/containers/BaseNodeContainer/index.tsx
+++ applications/launchpad_v2/src/containers/BaseNodeContainer/index.tsx
@@ -26,13 +26,26 @@ const BaseNodeContainer = () => {
   return (
     <div>
       <h2>Base Node</h2>
-      <Select
-        value={tariNetwork}
-        options={networkOptions}
-        onChange={setTariNetwork}
-        label="Tari network"
-        fullWidth
-      />
+      <div style={{padding: '16px'}}>
+        <Select
+          value={tariNetwork}
+          options={networkOptions}
+          onChange={setTariNetwork}
+          label="Tari network"
+        />
+      </div>
+
+
+      <div style={{backgroundColor: '#662FA1', padding: '16px'}}>
+        <Select
+          value={tariNetwork}
+          options={networkOptions}
+          onChange={setTariNetwork}
+          label="Tari network"
+          inverted
+        />
+      </div>
+
       <p>
         available docker images:
         <br />
