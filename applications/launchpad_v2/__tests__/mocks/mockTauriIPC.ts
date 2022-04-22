ebebd7cfb5e889daf8c22070c225b59ba428339e
tomaszantas <tom@altalogy.com>
2022-04-22 14:26:24 +0200

    Fix lint issues

--- applications/launchpad_v2/__tests__/mocks/mockTauriIPC.ts
+++ applications/launchpad_v2/__tests__/mocks/mockTauriIPC.ts
@@ -8,21 +8,21 @@ export const defaultTauriMockValues: Record<string, unknown> = {
     arch: 'x86_64',
     platform: 'darwin',
     ostype: 'Darwin',
-  }
+  },
 }
 
 /**
  * The Tauri IPC mock.
- * 
+ *
  * It uses Tauri's mockIPC and returns the value set in the `props`.
  * If nothing found in `props`, it will return a value from from `defaultTauriMockValues`.
- * 
+ *
  * @param {Record<string, unknown>} props - pass the value you expect in tests
- * 
+ *
  * @example
  * // Use default values:
  * tauriIPCMock()
- * 
+ *
  * // Get given value from specific API module (ie. 'platform' from 'os' module)
  * tauriIPCMock({
  *   os: {
@@ -43,14 +43,25 @@ export const tauriIPCMock = (props: Record<string, unknown> = undefined) => {
   })
 }
 
-const tauriCmdMock = (cmd: string, args: Record<string, unknown>, props: Record<string, unknown>) => {
+const tauriCmdMock = (
+  cmd: string,
+  args: Record<string, unknown>,
+  props: Record<string, unknown>,
+) => {
   const tauriModule = (args?.__tauriModule as string)?.toLowerCase()
   const messageCmd = (args?.message as { cmd?: string })?.cmd?.toLowerCase()
 
   if (tauriModule && messageCmd) {
-    if (props && Object.keys(props).includes(tauriModule) && Object.keys(props[tauriModule ]).includes(messageCmd)) {
-      return props[tauriModule][messageCmd ]
-    } else if (Object.keys(defaultTauriMockValues).includes(tauriModule) && Object.keys(defaultTauriMockValues[tauriModule]).includes(messageCmd)) {
+    if (
+      props &&
+      Object.keys(props).includes(tauriModule) &&
+      Object.keys(props[tauriModule]).includes(messageCmd)
+    ) {
+      return props[tauriModule][messageCmd]
+    } else if (
+      Object.keys(defaultTauriMockValues).includes(tauriModule) &&
+      Object.keys(defaultTauriMockValues[tauriModule]).includes(messageCmd)
+    ) {
       return defaultTauriMockValues[tauriModule][messageCmd]
     }
   }
