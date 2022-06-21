6a3992ba2a3618d1359602335ca2be3f2126591e
Alexander Todorov <matkat@tari.com>
2022-06-21 11:40:20 +0300

    Fixing cargo build

--- applications/launchpad/backend/src/docker/models.rs
+++ applications/launchpad/backend/src/docker/models.rs
@@ -230,7 +230,9 @@ impl ImageType {
             Self::Sha3Miner => "SHA3 miner",
             Self::MmProxy => "MM proxy",
             Self::Monerod => "Monerod",
-            Self::Frontail => "Frontail",
+            Self::Loki => "loki",
+            Self::Promtail => "promtail",
+            Self::Grafana => "grafana",
         }
     }
 
