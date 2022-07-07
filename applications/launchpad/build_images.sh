5d11a67852f408237c4f9b1c9b8b36a81dc35768
C.Lee Taylor <leet@tari.com>
2022-07-07 14:43:13 +0200

    Add latest tag/version/alias env support - Plus some tweaks

--- applications/launchpad/build_images.sh
+++ applications/launchpad/build_images.sh
@@ -30,14 +30,18 @@ build_3dparty_image() {
 
   echo "Building from $1 in $2 image version ${SUBTAG}${SUBTAG_EXTRA} ..."
 
+  # Add docker tag alias - ie: latest
+  if [ ! -z "${TL_TAG_ALIAS}" ]; then
+    TL_TAG_CMD=" -t ${TL_TAG_URL}/$2:${TL_TAG_ALIAS} "
+  fi
+
   docker ${TL_TAG_BUILD_OPTS} \
     -f docker_rig/$1 \
     --build-arg VERSION="${TL_VERSION}" \
     --build-arg ${2^^}_VERSION=${SUBTAG} \
     $4 $5 $6 $7 $8 $9 \
-    -t ${TL_TAG_URL}/$2:${SUBTAG}${SUBTAG_EXTRA} $3 ${TL_TAG_BUILD_Extra}
+    -t ${TL_TAG_URL}/$2:${SUBTAG}${SUBTAG_EXTRA} $3 ${TL_TAG_BUILD_Extra} ${TL_TAG_CMD}
 }
-
 
 build_tari_image() {
 # $1 image name
@@ -47,6 +51,11 @@ build_tari_image() {
 # $5 APP_EXEC ie: tari_base_node
 
   echo "Building from tarilabs.Dockerfile in $1 image version $2 ..."
+  # Add docker tag alias - ie: latest
+  if [ ! -z "${TL_TAG_ALIAS}" ]; then
+    TL_TAG_CMD=" -t ${TL_TAG_URL}/$1:${TL_TAG_ALIAS} "
+  fi
+
   docker ${TL_TAG_BUILD_OPTS} \
     -f docker_rig/tarilabs.Dockerfile \
     --build-arg ARCH=${TBN_ARCH} \
@@ -55,7 +64,7 @@ build_tari_image() {
     --build-arg APP_NAME=$4 \
     --build-arg APP_EXEC=$5 \
     $6 $7 $8 $9 \
-    -t ${TL_TAG_URL}/$1:$2 $3 ${TL_TAG_BUILD_Extra}
+    -t ${TL_TAG_URL}/$1:$2 $3 ${TL_TAG_BUILD_Extra} ${TL_TAG_CMD}
 }
 
 build_3dparty_image_json() {
@@ -121,13 +130,12 @@ if [ -f ".env.local" ]; then
   source ".env.local"
 fi
 
-#TL_TAG_BUILD_PF=amd64
-
 # Version refers to the base_node, wallet, etc.
 #  applications/tari_app_utilities/Cargo.toml
 TL_VERSION=${TL_VERSION:-$(awk -F ' = ' '$1 ~ /version/ \
   { gsub(/["]/, "", $2); printf("%s",$2) }' "../tari_base_node/Cargo.toml")}
 
+# Default build options - general x86-64 / AMD64
 TBN_ARCH=${TBN_ARCH:-x86-64}
 TBN_FEATURES=${TBN_FEATURES:-safe}
 
@@ -137,11 +145,17 @@ TL_TAG_URL=${TL_TAG_URL:-quay.io/tarilabs}
 # Docker build options
 TL_TAG_BUILD_OPTS=${TL_TAG_BUILD_OPTS:-"build"}
 
+# Docker tag suffix for platform
+#TL_TAG_BUILD_PF=-amd64
+
 #TL_VERSION_LONG="${TL_VERSION}-${TBN_ARCH}-${TBN_FEATURES}"
 TL_VERSION_LONG=${TL_VERSION_LONG:-"${TL_VERSION}${TL_TAG_BUILD_PF}"}
 
-# Sub Tag extra
+# Docker sub Tag extra
 #SUBTAG_EXTRA=${SUBTAG_EXTRA:-"-$TL_VERSION_LONG"}
+
+# Docker tag alias
+#TL_TAG_ALIAS=latest
 
 arrAllTools=(  $(jq -r '.[].image_name' tarisuite.json 3rdparty.json) )
 arrTariSuite=( $(jq -r '.[].image_name' tarisuite.json) )
@@ -168,18 +182,18 @@ case $commandEnv in
   -a | all )
     build_all_images
     ;;
-  build | -b )
+  -b | build )
     echo "Build a docker image"
     shift
     if [[ ${arrAllTools[*]} =~ (^|[[:space:]])"${1}"($|[[:space:]]) ]]; then
-      echo "Found $1"
+      echo "Image found - $1"
       if [ "${1:0:5}" == "tari_" ]; then
         build_tari_image_json $1
       else
         build_3dparty_image_json $1
       fi
     else
-      echo "Not found $1"
+      echo "Image not found for $1"
       build_help_info
       build_help_images
       exit 2
